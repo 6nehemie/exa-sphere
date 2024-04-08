0e58e2e6e6e2129e83a90069dfe43cce72767b33
NehemieMbg <nehemie.mbg@gmail.com>
2024-04-08 20:59:56 +0200

    Add constant to front end

---
+++ client/constants/index.ts
@@ -0,0 +1,62 @@
+export const auth = {
+  testimonial: `“Exa Sphere has been instrumental in streamlining my job application process. Their AI-generated cover letters, tailored to my job experience and the specific field I'm applying to, have been incredibly effective. It's like having a personal career advisor guiding me towards success.”`,
+  author: 'Naomi Liu',
+};
+
+export const login = {
+  title: 'Log into your account',
+  description: 'Enter your credentials below to login your account',
+  btn: 'Sign In',
+};
+
+export const register = {
+  title: 'Create an account',
+  description: 'Enter your email below to create your account',
+  btn: 'Sign up with email',
+};
+
+export const navigation = [
+  { label: 'My Profiles', href: '/my-profiles' },
+  { label: 'Generate', href: '/generate' },
+];
+
+export const settingsNavigation = [
+  { label: 'Profile', href: '/settings/profile' },
+  { label: 'Security', href: '/settings/security' },
+];
+
+export const jobProfile = {
+  title: 'Job Profile',
+  description:
+    'Create your new profile! Your profile will help tailor cover letters to suit your skills and experiences.',
+};
+
+export const jobProfiles = {
+  title: 'Job Profiles',
+  description:
+    'You can create, modify, and update up to 5 job profiles. Each profile allows you to showcase different aspects of your experience and skills. Feel free to manage your profiles to best represent your qualifications and career aspirations.',
+};
+
+export const education = {
+  title: 'Education',
+  description:
+    'Add your educational background to showcase your qualifications and achievements',
+};
+
+export const generate = {
+  title: 'Generate Your Cover Letter',
+  description:
+    'Create customized cover letters tailored to your skills and experiences. Simply select a profile and job, and let us assist you in crafting the perfect cover letter for your application.',
+};
+
+export const settingsProfile = {
+  title: 'Profile Details',
+  description:
+    'Update your personal information such as name, addresses, avatar, or email to ensure your profiles are accurate and up-to-date.',
+};
+
+export const settingsSecurity = {
+  title: 'Security',
+  description:
+    'Update your password to ensure your account is secure and protected.',
+};
