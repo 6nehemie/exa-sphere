7ffc43f26ff5d3a9da46c0f18bd11048fb335429
NehemieMbg <nehemie.mbg@gmail.com>
2024-04-09 17:54:13 +0200

    Adds documentation to fn

--- client/utils/functions/toInitials.ts
+++ client/utils/functions/toInitials.ts
@@ -1,3 +1,8 @@
+/**
+ * function to get the initials of a user
+ * @param name The full name of the user
+ * @returns the initials of the user (e.g. John Doe => JD)
+ */
 const toInitials = (name: string) => {
   return name
     .split(' ')
