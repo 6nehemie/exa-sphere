d8f702577858568b4271acfd565ec54b6ab2f30e
NehemieMbg <nehemie.mbg@gmail.com>
2024-04-12 01:24:52 +0200

    Adds server to project

---
+++ server/src/main/java/_6nehemie/com/server/model/User.java
@@ -0,0 +1,20 @@
+package _6nehemie.com.server.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+public class User {
+    
+    @Id
+    private Long id;
+}
