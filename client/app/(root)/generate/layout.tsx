483c08d594113e29aceeace684f56fed07ee3c57
NehemieMbg <nehemie.mbg@gmail.com>
2024-05-26 12:05:24 +0200

    Updates: Generate Ui

--- client/app/(root)/generate/layout.tsx
+++ client/app/(root)/generate/layout.tsx
@@ -2,11 +2,9 @@ import { ReactNode } from 'react';
 
 const GenerateLayout = async ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-[1300px]:gap-5 w-full">
-      <section className="w-full pb-8 h-full">
-        <div className="max-w-[778px] w-full mx-auto h-full">{children}</div>
-      </section>
-    </div>
+    <section className="generate-section max-w-[778px] w-full mx-auto">
+      {children}
+    </section>
   );
 };
 export default GenerateLayout;
--- client/components/forms/GenerateForm.tsx
+++ client/components/forms/GenerateForm.tsx
@@ -82,210 +82,210 @@ const GenerateForm = ({ profiles }: { profiles: Profile[] }) => {
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="generate-new-grid"
-      >
-        <div className="space-y-10 mb-12">
-          <h1 className="text-5xl font-roboto space-y-2">
-            <span className="block text-gray-exa-1">
-              {generate.title.part1}, {user.firstName}
-            </span>
-            <span className="block text-gray-highlight-1">
-              {generate.title.part2}
-            </span>
-          </h1>
+    <div className="generate-new-grid gap-y-20">
+      <h1 className="text-5xl font-roboto space-y-2 mt-14 md:mt-[68px]">
+        <span className="block text-gray-exa-1">
+          {generate.title.part1}, {user.firstName}
+        </span>
+        <span className="block text-gray-highlight-1">
+          {generate.title.part2}
+        </span>
+      </h1>
 
-          <div className="space-y-4">
-            <div className="grid md:grid-cols-2 gap-5">
-              <FormField
-                control={form.control}
-                name={`jobTitle`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Job Title{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div>
-                        <Input {...field} />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`company`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Company{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div>
-                        <Input {...field} />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="grid md:grid-cols-2 gap-5">
-              <FormField
-                control={form.control}
-                name={`location`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Location{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div>
-                        <Input {...field} />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`jobType`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Job Type{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div>
-                        <Input {...field} />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="grid md:grid-cols-2 gap-5">
-              <FormField
-                control={form.control}
-                name={`experienceLevel`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Experience Level{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div>
-                        <Input {...field} />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`profileId`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="">
-                      Select Profile{' '}
-                      <span className="text-gray-exa-2">(required)</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={(value) =>
-                          form.setValue('profileId', value)
-                        }
-                      >
-                        <SelectTrigger className="w-full bg-transparent border-gray-2 text-sm font-light">
-                          <SelectValue placeholder="No Profile Selected" />
-                        </SelectTrigger>
-                        <SelectContent className="bg-gray-3 border-gray-2 text-white">
-                          <SelectGroup>
-                            <SelectLabel className="font-normal">
-                              {profiles.length > 0
-                                ? 'Profiles'
-                                : 'A profile is required to generate a cover letter.'}
-                            </SelectLabel>
-                            {profiles.map((profile) => (
-                              <SelectItem
-                                key={profile.id}
-                                value={`${profile.id}`}
-                                className="text-sm font-light"
-                              >
-                                <SelectLabel className="font-light text-sm px-0 text-start">
-                                  {profile.title}
-                                </SelectLabel>
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 self-end"
+        >
+          <div className="grid md:grid-cols-2 gap-5">
             <FormField
               control={form.control}
-              name={`description`}
+              name={`jobTitle`}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="">
-                    Job Description{' '}
+                    Job Title{' '}
                     <span className="text-gray-exa-2">(required)</span>
                   </FormLabel>
                   <FormControl>
                     <div>
-                      <Textarea
-                        rows={12}
-                        placeholder="Type your message here."
-                        {...field}
-                      />
+                      <Input {...field} />
                     </div>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-
-            <div className="flex justify-end w-full mt-12">
-              <Button
-                type="submit"
-                disabled={isLoading}
-                className="rounded-full h-10 bg-gray-highlight-1 text-gray-1 hover:bg-gray-exa-3 font-light"
-              >
-                <Loader2
-                  className={cn('mr-2 h-4 w-4 animate-spin font-light', {
-                    hidden: !isLoading,
-                  })}
-                />
-                <span>
-                  {isLoading
-                    ? 'Generating Cover Letter...'
-                    : 'Generate Cover Letter'}
-                </span>
-              </Button>
-            </div>
+            <FormField
+              control={form.control}
+              name={`company`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="">
+                    Company <span className="text-gray-exa-2">(required)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <div>
+                      <Input {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
-        </div>
-      </form>
-    </Form>
+
+          <div className="grid md:grid-cols-2 gap-5">
+            <FormField
+              control={form.control}
+              name={`location`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="">
+                    Location <span className="text-gray-exa-2">(required)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <div>
+                      <Input {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={`jobType`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="">
+                    Job Type <span className="text-gray-exa-2">(required)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <div>
+                      <Input {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="grid md:grid-cols-2 gap-5">
+            <FormField
+              control={form.control}
+              name={`experienceLevel`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="">
+                    Experience Level{' '}
+                    <span className="text-gray-exa-2">(required)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <div>
+                      <Input {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={`profileId`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="">
+                    Select Profile{' '}
+                    <span className="text-gray-exa-2">(required)</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={(value) =>
+                        form.setValue('profileId', value)
+                      }
+                    >
+                      <SelectTrigger className="w-full bg-transparent border-gray-2 text-sm font-light">
+                        <SelectValue placeholder="No Profile Selected" />
+                      </SelectTrigger>
+                      <SelectContent className="bg-gray-3 border-gray-2 text-white">
+                        <SelectGroup>
+                          <SelectLabel className="font-normal">
+                            {profiles.length > 0
+                              ? 'Profiles'
+                              : 'A profile is required to generate a cover letter.'}
+                          </SelectLabel>
+                          {profiles.map((profile) => (
+                            <SelectItem
+                              key={profile.id}
+                              value={`${profile.id}`}
+                              className="text-sm font-light"
+                            >
+                              <SelectLabel className="font-light text-sm px-0 text-start">
+                                {profile.title}
+                              </SelectLabel>
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name={`description`}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="">
+                  Job Description{' '}
+                  <span className="text-gray-exa-2">(required)</span>
+                </FormLabel>
+                <FormControl>
+                  <div>
+                    <Textarea
+                      rows={12}
+                      placeholder="Type your message here."
+                      {...field}
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex flex-col items-end mt-12 py-4 space-y-2">
+            <Button
+              type="submit"
+              disabled={isLoading}
+              className="max-md:w-full ml-auto rounded-full h-10 bg-gray-highlight-1 text-gray-exa-1 hover:bg-gray-exa-3"
+            >
+              <Loader2
+                className={cn('mr-2 h-4 w-4 animate-spin font-light', {
+                  hidden: !isLoading,
+                })}
+              />
+              <span>
+                {isLoading
+                  ? 'Generating Cover Letter...'
+                  : 'Generate Cover Letter'}
+              </span>
+            </Button>
+
+            <p className="font-light text-gray-exa-2 max-md:mx-auto">
+              Exa Sphere can make mistakes. Please review your cover letter
+              carefully before using it.
+            </p>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 };
 export default GenerateForm;
